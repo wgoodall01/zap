7b9d4c87ff0438bfec97f4dd963d7b061e78240e
William Goodall <wgoodall01@gmail.com>
2025-08-16 21:20:59 -0400

    api: bootstrap

---
+++ pkg/api/src/main.rs
@@ -0,0 +1,122 @@
+use rocket::form::FromForm;
+use rocket::{get, post, serde::json::Json};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    /// A unique user identifier.
+    user_id: u64,
+    /// The current username of the user.
+    username: String,
+    #[schemars(example = "example_email")]
+    email: Option<String>,
+}
+
+fn example_email() -> &'static str {
+    "test@example.com"
+}
+
+/// # Get all users
+///
+/// Returns all users in the system.
+#[openapi(tag = "Users")]
+#[get("/user")]
+fn get_all_users() -> Json<Vec<User>> {
+    Json(vec![User {
+        user_id: 42,
+        username: "bob".to_owned(),
+        email: None,
+    }])
+}
+
+/// # Get user
+///
+/// Returns a single user by ID.
+#[openapi(tag = "Users")]
+#[get("/user/<id>")]
+fn get_user(id: u64) -> Option<Json<User>> {
+    Some(Json(User {
+        user_id: id,
+        username: "bob".to_owned(),
+        email: None,
+    }))
+}
+
+/// # Get user by name
+///
+/// Returns a single user by username.
+#[openapi(tag = "Users")]
+#[get("/user_example?<user_id>&<name>&<email>")]
+fn get_user_by_name(user_id: u64, name: String, email: Option<String>) -> Option<Json<User>> {
+    Some(Json(User {
+        user_id,
+        username: name,
+        email,
+    }))
+}
+
+/// # Create user
+#[openapi(tag = "Users")]
+#[post("/user", data = "<user>")]
+fn create_user(user: Json<User>) -> Json<User> {
+    user
+}
+
+#[openapi(skip)]
+#[get("/hidden")]
+fn hidden() -> Json<&'static str> {
+    Json("Hidden from swagger!")
+}
+
+#[derive(Serialize, Deserialize, JsonSchema, FromForm)]
+struct Post {
+    /// The unique identifier for the post.
+    post_id: u64,
+    /// The title of the post.
+    title: String,
+    /// A short summary of the post.
+    summary: Option<String>,
+}
+
+/// # Create post using query params
+///
+/// Returns the created post.
+#[openapi(tag = "Posts")]
+#[get("/post_by_query?<post..>")]
+fn create_post_by_query(post: Post) -> Option<Json<Post>> {
+    Some(Json(post))
+}
+
+#[rocket::main]
+async fn main() {
+    let launch_result = rocket::build()
+        .mount(
+            "/",
+            openapi_get_routes![
+                get_all_users,
+                get_user,
+                get_user_by_name,
+                create_user,
+                hidden,
+                create_post_by_query,
+            ],
+        )
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
+}
